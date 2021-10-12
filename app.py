2fb06be4ce5732c2a49f5d3f1e99c4ad7b263f06
Oliver Cadman <o.cadman@live.co.uk>
2021-10-12 09:48:22 +0100

    Initialize Flask application

---
+++ app.py
@@ -0,0 +1,21 @@
+"""
+Initialise and configure the Flask Application
+"""
+
+import os
+from flask import Flask
+if os.path.exists("env.py"):
+    import env
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "running"
+
+if __name__ == "__main__":
+    app.run(host = os.environ.get("IP"),
+            port = int(os.environ.get("PORT")),
+            debug=True)
+
+
