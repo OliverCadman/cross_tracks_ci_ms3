b61a5ec2d019a8a38011e289d46b7d351676cb45
Oliver Cadman <o.cadman@live.co.uk>
2021-10-13 11:26:51 +0100

    Refactor flask initialisation following Flask Factory method

---
+++ application/__init__.py
@@ -0,0 +1,32 @@
+"""
+Flask Application Factory
+=========================
+
+Instantiate Flask Application with default confguration
+and initialises MongoDB.
+
+"""
+
+from flask import Flask
+from flask_pymongo import PyMongo
+from application.config import Config
+
+# Instantiate Mongo Database and assign to variable
+mongo = PyMongo()
+
+def create_app(default_config=Config):
+    """
+    Creates the application factory.
+    """
+
+    app = Flask(__name__)
+    app.config.from_object(default_config)
+
+    # Initialise Mongo Database
+    mongo.init_app(app)
+
+    return app
+
+
+
+
