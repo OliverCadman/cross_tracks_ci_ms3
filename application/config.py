3b3f546f1f08c100023cc6f9665c7fcf07043df1
Oliver Cadman <o.cadman@live.co.uk>
2021-10-13 11:25:17 +0100

    Create config.py file to handle configuration variables for MongoDB

---
+++ application/config.py
@@ -0,0 +1,25 @@
+"""
+Sets the configuration variables needed for
+the application to run. 
+
+class 'Config' passed into create_app()
+function in "application/__init__.py"
+
+"""
+
+import os
+if os.path.exists("env.py"):
+    import env
+
+class Config:
+    # MongoDB config
+    SECRET_KEY = os.environ.get("SECRET_KEY")
+    MONGO_URI = os.environ.get("MONGO_URI")
+    MONGO_DBNAME = os.environ.get("MONGO_DBNAME")
+    
+
+
+
+
+
+
