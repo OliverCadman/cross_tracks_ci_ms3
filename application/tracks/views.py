51b7ffd1bdd22da63aece9d14fe242035947ded6
Oliver Cadman <o.cadman@live.co.uk>
2021-10-20 11:37:07 +0100

    Create post request, creating new Track object from form data in add-track.html, and inserting to Mongo.

--- application/tracks/views.py
+++ application/tracks/views.py
@@ -10,4 +10,18 @@ def add_track(username):
     user_id = User.get_id(username)
     genres = Track.get_genres()
 
+    if request.method == "POST":
+
+        track_name = request.form.get("track_name")
+        artist_name = request.form.get("artist_name")
+        album_name = request.form.get("album_name")
+        genre = request.form.get("genre")
+        year_of_release = request.form.get("year_of_release")
+        added_by = user_id
+        
+        new_track = Track(track_name, artist_name, album_name,
+                          genre, year_of_release, added_by)
+
+        new_track.add_track()
+
     return render_template("add-track.html", genres=genres, user_id=user_id)
