4c17bf9abb30f98fed9b3faa6ea9f9cf302950ff
Oliver Cadman <o.cadman@live.co.uk>
2021-10-20 11:36:04 +0100

    Create function add_track, invoke functions get_id and get_genres, assigned to variables

--- application/tracks/views.py
+++ application/tracks/views.py
@@ -1,8 +1,13 @@
 from flask import (Blueprint, render_template, url_for, flash, request, session)
 from application.tracks.classes import Track
+from application.users.classes import User
 
 tracks = Blueprint("tracks", __name__)
 
-@tracks.route("/add-track")
-def add_track():
-    return render_template("add-track.html")
+@tracks.route("/add-track/<username>", methods=["GET","POST"])
+def add_track(username):
+
+    user_id = User.get_id(username)
+    genres = Track.get_genres()
+
+    return render_template("add-track.html", genres=genres, user_id=user_id)
