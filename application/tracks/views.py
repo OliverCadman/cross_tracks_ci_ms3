bf52347d7fd3a0440e1dbbe2104af78fb443384c
Oliver Cadman <o.cadman@live.co.uk>
2021-10-21 17:11:17 +0100

    Pass Track.get_latest_tracks into browse-tracks route, using variable latest_tracks

--- application/tracks/views.py
+++ application/tracks/views.py
@@ -17,11 +17,24 @@ def add_track(username):
         album_name = request.form.get("album_name")
         genre = request.form.get("genre")
         year_of_release = request.form.get("year_of_release")
+        image_url = request.form.get("image_url")
         added_by = user_id
         
         new_track = Track(track_name, artist_name, album_name,
-                          genre, year_of_release, added_by)
+                          genre, year_of_release, added_by, image_url)
 
         new_track.add_track()
 
     return render_template("add-track.html", genres=genres, user_id=user_id)
+
+
+@tracks.route("/browse-tracks")
+def browse_tracks():
+
+    all_tracks = Track.get_all_tracks()
+
+    latest_tracks = Track.get_latest_tracks()
+
+    return render_template("browse-tracks.html", all_tracks=all_tracks,
+                            latest_tracks=latest_tracks)
+    
