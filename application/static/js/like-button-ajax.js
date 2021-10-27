564f63ef174a599ecdfa733ddbcff1ba876aa311
Oliver Cadman <o.cadman@live.co.uk>
2021-10-27 12:35:17 +0100

    Create if statement to check if user who liked tracks is in the list of likes associated with track, and replace fontawesome star outline class with filled star class, and vice versa

--- application/static/js/like-button-ajax.js
+++ application/static/js/like-button-ajax.js
@@ -1,3 +1,8 @@
+/* JavaScript hooked up to templates/browse-tracks.html
+Loops through anchor tags with href '{{url_for('tracks.like_track)}}'
+and uses AJAX to call the like_track function, and update the DOM 
+asynchronously */
+
 document.addEventListener("DOMContentLoaded", function () {
   collectLikeButtons();
 });
@@ -19,11 +24,26 @@ function addLikeAndUpdate(e) {
   $.ajax(this.href, {
     success: function (res) {
         console.log(res)
+        // Update the DOM element displaying number of likes
         let numLikesContainer = e.target.parentElement.parentElement.parentElement.childNodes[4]
         if (res === 0) {
             numLikesContainer.innerHTML = ''
         } else {
-            numLikesContainer.innerHTML = `${res}`
+            numLikesContainer.innerHTML = `${res.num_of_likes}`
+        }
+
+        let currentUser = res.username
+        let listOfLikes = res.likes_list
+        let likeIcon = e.target.parentElement.children[0];
+        
+        /* Replace FontAwesome star icon outline with 
+        filled star if track is liked, and vice versa */
+        if (listOfLikes.includes(currentUser)) {
+          likeIcon.classList.remove('far', 'fa-star')
+          likeIcon.classList.add('fas', 'fa-star')
+        } else {
+          likeIcon.classList.remove('fas', 'fa-star')
+          likeIcon.classList.add('far', 'fa-star')
         }
     },
   });
