56ef260c9cf8cac70b2b7a52c150a6f3545209c0
Oliver Cadman <o.cadman@live.co.uk>
2021-10-29 13:20:22 +0100

    Add method 'add_comment', inserting new comment object into MongoDB 'comments' collection

--- application/comments/classes.py
+++ application/comments/classes.py
@@ -25,3 +25,27 @@ class Comment:
         return comment_info
 
     
+    def get_comment_info(self):
+
+        date_added = datetime.datetime.now()
+
+        comment_info = {
+            "comment": self.comment_body,
+            "author": self.comment_author,
+            "track_id": self.track_id,
+            "date_added": date_added
+        }
+
+        return comment_info
+
+    
+    def add_comment(self):
+
+
+        comment = self.get_comment_info()
+
+        if comment:
+            try:
+                mongo.db.comments.insert_one(comment)
+            except:
+                print('Server error, unable to insert document')
