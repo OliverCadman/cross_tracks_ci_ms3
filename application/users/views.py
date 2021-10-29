61f3bb90dea01de9f935321e6f3636f30dc9eb8e
Oliver Cadman <o.cadman@live.co.uk>
2021-10-29 11:35:11 +0100

    Created function 'edit_profile', handling form input from modal in 'user-profile.html' and updating MongoDB with 'User.edit_profile()'

--- application/users/views.py
+++ application/users/views.py
@@ -23,6 +23,7 @@ from time import time
 from flask_mail import Message
 from application import mailing
 import jwt
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 if os.path.exists("env.py"):
     import env
 
@@ -74,63 +75,57 @@ def register_user():
 
 
 
-@users.route("/build-profile/<username>", methods=["GET", "POST"])
+@users.route("/profile-edit/<username>", methods=["GET", "POST"])
 def build_profile(username):
     """
     build_profile() runs after initial registration, to gather information
     to be displayed on user's profile page.
     """
-    
 
     if request.method == "POST":
 
         if 'profile_image' in request.files:
 
-            profile_image = request.files['profile_image']
-            
-            profile_image.seek(0, os.SEEK_END)
+            allowed_filesize = User.check_image_filesize(request.cookies.get('filesize'))
+            if not allowed_filesize:
+                flash('Your file is too large!')
+                return redirect(url_for("users.build_profile", username=session["user"]))
 
-            if profile_image.tell() == 0:
-                print("no file")
-            
-            elif profile_image:
+            profile_image = request.files['profile_image']
+            if profile_image:
+
+                if profile_image.filename == '':
+                    flash("Your image must have a filename!")
+                    return redirect(url_for('users.build_profile', username=session["user"]))
 
                 allowed_image = User.allowed_file(profile_image.filename)
-                allowed_filesize = User.check_image_filesize(request.cookies.get('filesize'))
-                
-                if not allowed_filesize:
-                    flash('Your file is too large!')
-                    return redirect(url_for("users.build_profile", username=session["user"]))
 
-                elif not allowed_image:
+                if not allowed_image:
                     flash("Images can have extensions 'jpg', 'jpeg', 'gif', 'png' and 'pdf' only.")
-                    return redirect(url_for('users.build_profile', username=session["user"]))
+                    return redirect(url_for('users.build_profile', username=username))
 
                 else:
                     mongo.save_file(profile_image.filename, profile_image)
-                    profile_image_info = {
-                        "profile_image": profile_image.filename
-                    }
-                    User.complete_user_profile(username, profile_image_info)
                 
                 
-        profile_info = {
-            "first_name": request.form.get("first_name"),
-            "last_name": request.form.get("last_name"),
-            "date_of_birth": request.form.get("date_of_birth"),
-            "city": request.form.get("city"),
-            "country": request.form.get("country"),
-            "about_user": request.form.get("about_user"),
-            "spotify_userID": request.form.get("spotify_userID"),
-            "display_spotify_playlists": request.form.get("display_spotify_playlists"),
-            "is_artist": request.form.get("is_artist"),
-            }
+            profile_info = {
+                "first_name": request.form.get("first_name"),
+                "last_name": request.form.get("last_name"),
+                "date_of_birth": request.form.get("date_of_birth"),
+                "city": request.form.get("city"),
+                "country": request.form.get("country"),
+                "about_user": request.form.get("about_user"),
+                "spotify_userID": request.form.get("spotify_userID"),
+                "display_spotify_playlists": request.form.get("display_spotify_playlists"),
+                "is_artist": request.form.get("is_artist"),
+                "profile_image": profile_image.filename
+                }
 
-        User.complete_user_profile(username, profile_info)
+            User.complete_user_profile(username, profile_info)
 
-        return redirect(url_for("users.user_profile", username = username))
+            return redirect(url_for("users.user_profile", username = username))
         
-    return render_template('build-profile.html', username=username)
+    return render_template('build-profile.html')
 
 
 
@@ -157,7 +152,42 @@ def login():
             flash("Invalid username/password")
             return redirect(url_for("users.login"))
 
-    return render_template("login.html")       
+    return render_template("login.html")   
+
+
+@users.route("/edit-profile/<username>", methods = ["GET", "POST"])
+def edit_profile(username):
+
+    user = User.get_user(username)
+
+    if request.method == "POST":
+        
+        if not username:
+            flash("You need to be logged in to edit your profile")
+            return redirect("users.login")
+
+        display_spotify_playlists = "on" if request.form.get("display_spotify_playlists") else "off"
+        is_artist = "on" if request.form.get("is_artist") else "off"
+
+        edited_info = {
+            "first_name": request.form.get("first_name"),
+            "last_name": request.form.get("last_name"),
+            "date_of_birth": request.form.get("date_of_birth"),
+            "city": request.form.get("city"),
+            "country": request.form.get("country"),
+            "about_user": request.form.get("about_user"),
+            "spotify_userID": request.form.get("spotify_userID"),
+            "display_spotify_playlists": display_spotify_playlists,
+            "is_artist": is_artist
+        }
+
+        user.edit_profile(username, edited_info)
+        session["user"] = user.username
+        flash("Your profile has been updated")
+        return redirect(url_for("users.user_profile", username=session["user"]))
+
+
+
         
                
 
@@ -171,8 +201,14 @@ def logout():
 
 @users.route("/file/<path:filename>")
 def display_profile_image(filename):
+
+    try:
+        profile_image = mongo.send_file(filename)
+        return profile_image
+    except Exception as e:
+        print(f"Error uploading image: {e}")
+        return None
  
-    return mongo.send_file(filename)
 
 
 @users.route("/user-profile/<username>")
@@ -226,15 +262,17 @@ def user_profile(username):
 
 # https://medium.com/@stevenrmonaghan/password-reset-with-flask-mail-protocol-ddcdfc190968
 
-def get_user_token(email, expires=500):
+def get_reset_token(email, expires_sec=500):
     """
     Generates and returns JSON Web Token to be used for authorization
     """
-    return jwt.encode({
-        'reset_password': email,
-        'exp': time() + expires}, 
-         key = os.environ.get("SECRET_KEY"))
 
+    user = User.find_user_by_email(email)
+    user_id = str(user["_id"])
+
+   
+    s = Serializer(os.environ.get("SECRET_KEY"), expires_sec)
+    return (s.dumps({"user_id": user_id}).decode('utf-8'))
 
 @users.route('/request-password-reset', methods=["GET", "POST"])
 def request_password_reset():
@@ -264,23 +302,71 @@ def request_password_reset():
         # Query MongoDB to check user input against email address in DB
         user = User.find_user_by_email(email_input)
         if not user:
-            flash("We're sorry, we couldn't find your email. ")
+            flash("We're sorry, we couldn't find your email.")
             return redirect("users.login")
 
-        token = get_user_token(email_input)
+        token = get_reset_token(email_input)
 
         msg = Message()
         msg.subject = "Cross//Tracks: Reset your Password"
         msg.sender = os.environ.get("MAIL_USERNAME")
         msg.recipients = [user["email_address"]]
-        msg.html = render_template('password-reset-email.html', token=token, user=user["first_name"])
+        msg.html = render_template('password-reset-email.html', 
+                                    token=token, 
+                                    user=user["first_name"].title())
         mailing.send(msg)
         
 
     return render_template('login.html')
 
-@users.route("/reset-password", methods=["GET", "POST"])
-def reset_password():
+def verify_reset_token(token):
+    """
+    Decodes the JSON web token
+    and return's user email address 
+    if successful. If unsuccessful,
+    (primarily due to the token timing out),
+    Flash message is displayed with relevant
+    error message and User is redirected to login 
+    page.
+    """
+
+    SECRET_KEY = os.environ.get("SECRET_KEY")
+    s = Serializer(SECRET_KEY)
+    
+    try:
+        user_id = s.loads(token)["user_id"]
+    except Exception as e:
+        print(e)
+        return None
+    
+    found_user = User.find_user_by_id(user_id)
+    return found_user
+    
+
+@users.route("/reset-password/<token>", methods=["GET", "POST"])
+def reset_password(token):
+
+    user = verify_reset_token(token)
+    
+
+    # if request.method == "POST":
+
+    #     password = request.form.get("password")
+    #     confirm_password = request.form.get("confirm_password")
+
+    #     if not User.validate_password_format(password):
+    #         flash("Please include at least one capital letter and one number")
+    #         return redirect(url_for("users.reset_password"))
+
+    #     if not User.validate_password_match(password, confirm_password):
+    #         flash("Passwords do not match!")
+    #         return redirect(url_for("users.reset_password"))
+
+        
+
+
+
+    
     return render_template('reset-password.html')
 
 
