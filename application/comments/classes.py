1d0ab212d97f231bd2e99f045a9400dc720a36fb
Oliver Cadman <o.cadman@live.co.uk>
2021-10-29 13:00:14 +0100

    Create Comment class, initialised with 'comment_body, comment_author, track_id, _id=None'

---
+++ application/comments/classes.py
@@ -0,0 +1,14 @@
+from application import mongo
+import datetime
+
+class Comment:
+
+    def __init__(self, comment_body, comment_author, track_id, _id=None):
+
+        self.comment_body = comment_body
+        self.comment_author = comment_author
+        self.track_id = track_id
+        self._id = _id if isinstance(_id, str) else ""
+
+    
+  
