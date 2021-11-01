504e9b99383d0f8dac6073abf7085cefbc4d0a3c
Oliver Cadman <o.cadman@live.co.uk>
2021-11-01 10:29:40 +0000

    Import and register comments blueprint

--- application/__init__.py
+++ application/__init__.py
@@ -9,16 +9,16 @@ and initialises MongoDB.
 
 from flask import Flask
 from flask_pymongo import PyMongo
+from flask_mail import Mail
 from application.config import Config
-from flask_login import LoginManager
-
 
 
 # Instantiate Mongo Database and assign to variable
 mongo = PyMongo()
 
-# Instantiate Flask Login Manager
-login_manager = LoginManager()
+# Instantiate Flask Mail and assign to variable
+mailing = Mail()
+
 
 def create_app(default_config=Config):
     """
@@ -27,22 +27,25 @@ def create_app(default_config=Config):
 
     app = Flask(__name__)
     app.config.from_object(default_config)
+   
 
     # Initialise Mongo Database
     mongo.init_app(app)
 
-    # Initialise Flask Login Manager
-    login_manager.init_app(app)
+    # Initialise Flask Mail
+    mailing.init_app(app)
 
 
     # Import and Register blueprints
     from application.main.views import main as main_bp
     from application.users.views import users as users_bp
     from application.tracks.views import tracks as tracks_bp
+    from application.comments.views import comments as comments_bp
     
     app.register_blueprint(main_bp)
     app.register_blueprint(users_bp)
     app.register_blueprint(tracks_bp)
+    app.register_blueprint(comments_bp)
 
 
     return app
