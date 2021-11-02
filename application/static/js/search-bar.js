de0b676e443f2dc897f9c102329a4bbcc67fb0a0
Oliver Cadman <o.cadman@live.co.uk>
2021-11-02 13:17:41 +0000

    Added file search-bar.js, with code to toggle dropdown overlay when Search icon clicked in 'browse-tracks.html'

--- application/static/js/search-bar.js
+++ application/static/js/search-bar.js
@@ -1,14 +1,32 @@
 $(document).ready(function() {
 
-    let searchBarButton = document.getElementById('search-bar-icon')
-    
-    searchBarButton.addEventListener("click", openSearchBar)
+    let searchBarButtons = document.getElementsByClassName('search-bar-icon')
 
+    for(let i = 0; i < searchBarButtons.length; i++){
+        
+         searchBarButtons[i].addEventListener("click", openSearchBar);
+        }
+
+    let closeSearchBarIcon = document.getElementById("close-search-wrapper");
+    closeSearchBarIcon.addEventListener("click", closeSearchBar) 
+    
 
 
     function openSearchBar() {
-        let searchBar = document.getElementById('search-bar-mobile-wrapper');
-
+        let searchBar = document.getElementById('search-bar-wrapper');
+        console.log('click')
         searchBar.classList.toggle('show-search-bar');
+    }
+
+    
+    function closeSearchBar(){
+        let searchBar = document.getElementById('search-bar-wrapper')
+        let resultsWrapper = document.getElementsByClassName("results-wrapper")[0]
+        let inputField = document.getElementById('search_input')
+
+        resultsWrapper.innerHTML = ""
+        inputField.value = ""
+
+        searchBar.classList.remove('show-search-bar')
     }
 })
