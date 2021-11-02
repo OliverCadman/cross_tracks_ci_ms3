9dfde19a989371e3b865e49215b34a0666cf286f
Oliver Cadman <o.cadman@live.co.uk>
2021-11-02 13:10:46 +0000

    No additions since last commit. Pushing for testing

--- application/static/js/initialization.js
+++ application/static/js/initialization.js
@@ -1,10 +1,16 @@
 /* File contains all code required to initialize
 MaterializeCSS features */
 $(document).ready(function () {
+  console.log("ready")
+  $(".modal").modal({
+    opacity: 0.7,
+  });
   $(".dropdown-trigger").dropdown();
   $(".sidenav").sidenav({
     edge: "right",
     inDuration: 500,
     draggable: true,
   });
+  $(".tooltipped").tooltip();
+  $("select").formSelect();
 });
