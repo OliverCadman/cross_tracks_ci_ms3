dc7d16968fdbb28c929b78cbfac5bd77da51fd5a
Oliver Cadman <o.cadman@live.co.uk>
2021-12-07 17:09:05 +0000

    Added functionality to send flask mail when 'Send Email' submit btn is clicked in contact.html. Created simply html document to contain email content

--- application/main/views.py
+++ application/main/views.py
@@ -1,4 +1,7 @@
-from flask import render_template, Blueprint
+from flask import render_template, url_for, Blueprint, request, flash, redirect
+from application import mailing
+from flask_mail import Message
+import os
 
 main = Blueprint("main",__name__)
 
@@ -7,6 +10,28 @@ main = Blueprint("main",__name__)
 def index():
     return render_template("index.html")
 
-@main.route("/contact")
+@main.route("/contact", methods=["GET", "POST"])
 def contact():
+        
     return render_template("contact.html")
+
+@main.route("/send_email", methods=["GET", "POST"])
+def send_email():
+
+    if request.method == "POST":
+
+        sender = request.form.get("full_name")
+        sender_email = request.form.get("email_address")
+        message = request.form.get("questions_comments")
+
+        msg = Message()
+        msg.subject = "Cross//Tracks Feedback/Query"
+        msg.sender = sender_email
+        msg.recipients = ['o.cadman@live.co.uk']
+        msg.html = render_template('contact-message.html', user='Oliver', sender_name=sender, sender_email=sender_email, message=message)
+
+        mailing.send(msg)
+
+        flash("Thanks for contacting us! We will reply shortly")
+        return redirect(url_for('main.contact'))
+
