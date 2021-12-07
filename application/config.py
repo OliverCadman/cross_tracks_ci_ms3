03aa9fcdcedc9444afb35bb831856984bb42252a
Oliver Cadman <o.cadman@live.co.uk>
2021-12-07 20:39:05 +0000

    Added Flask mail configuration

--- application/config.py
+++ application/config.py
@@ -17,6 +17,13 @@ class Config:
     MONGO_URI = os.environ.get("MONGO_URI")
     MONGO_DBNAME = os.environ.get("MONGO_DBNAME")
     
+    MAIL_SERVER = os.environ.get("MAIL_SERVER")
+    MAIL_PORT = os.environ.get("MAIL_PORT")
+    MAIL_USE_TLS = os.environ.get("MAIL_USE_TLS")
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    SENDINBLUE_API_KEY = os.environ.get("SENDINBLUE_API_KEY")
+    
 
 
 
