494272a399774751469c9537e7e2345913136feb
Oliver Cadman <o.cadman@live.co.uk>
2021-12-09 19:39:08 +0000

    Added functionality to check image filesize and alert user if file is too large, or submit form, when updated image file is selected

---
+++ application/static/js/update-profile-image.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+     document.getElementById("profile_image").onchange = function (e) {
+       e.preventDefault();
+
+       document.cookie = `filesize=${e.currentTarget.files[0].size}`;
+
+       console.log(document.cookie.split("=")[1]);
+
+       let allowedFilesize = 0.5 * 1024 * 1024;
+
+       if (document.cookie.split("=")[1] > allowedFilesize) {
+         imgTooLargeToast("Images must be less than 500KB");
+       } else {
+         document.getElementById("edit-profile-img-form").submit();
+       }
+     };
+})
