32370eb406c8383af0b7e9d131dc2062bd6bc2cf
Oliver Cadman <o.cadman@live.co.uk>
2021-12-10 10:20:57 +0000

    Register admin blueprint

--- application/__init__.py
+++ application/__init__.py
@@ -23,7 +23,8 @@ mailing = Mail()
 
 def create_app(default_config=Config):
     """
-    Creates the application factory.
+    Creates the application factory, with config
+    imported from from Config object in config.py.
     """
 
     app = Flask(__name__)
@@ -42,23 +43,34 @@ def create_app(default_config=Config):
     from application.users.views import users as users_bp
     from application.tracks.views import tracks as tracks_bp
     from application.comments.views import comments as comments_bp
+    from application.admin.views import admin as admin_bp
     
     app.register_blueprint(main_bp)
     app.register_blueprint(users_bp)
     app.register_blueprint(tracks_bp)
     app.register_blueprint(comments_bp)
+    app.register_blueprint(admin_bp)
+    
 
+    # Register 404 and 500 error handlers
     app.register_error_handler(404, page_not_found)
     app.register_error_handler(500, internal_error)
 
-
-
     return app
 
+
 def page_not_found(e):
+    """
+    Returns 404 page in case of invalid
+    URL path.
+    """
     return render_template("404.html"), 404
 
 def internal_error(e):
+    """
+    Returns 500 page in case of internal
+    server/mongoDB error
+    """
     return render_template("500.html"), 500
 
 
