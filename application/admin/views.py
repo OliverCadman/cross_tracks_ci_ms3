3fe722136986a4ee0889576b7f3af320a8f91832
Oliver Cadman <o.cadman@live.co.uk>
2021-12-10 11:08:58 +0000

    Added view 'delete_genre', with genre_id as parameter. Invokes Track.delete_genre method to query mongoDB, and delete genre by genre_id

--- application/admin/views.py
+++ application/admin/views.py
@@ -1,13 +1,48 @@
 from flask import (Blueprint, render_template, redirect,
-                   url_for, session)
+                   url_for, session, flash)
 from application.tracks.classes import Track
 
 admin = Blueprint('admin', __name__)
 
 @admin.route("/manage-genres")
 def manage_genres():
+    """
+    Manage genres view
 
-    all_genres = Track.get_genres()
-  
+    Queries mongoDB Genre collection for all genres,
+    which are passed in to the render_template return
+    function, to be rendered onto the 'manage-genres.html'
+    """
 
-    return render_template('manage-genres.html', all_genres=all_genres )
+    # Avoids display of page if any user other than
+    # admin attempts to manually enter URL path
+    if session["user"] == "admin":
+
+        all_genres = Track.get_genres()
+        return render_template('manage-genres.html', all_genres=all_genres)
+
+    else:
+        flash("You're not meant to be there!")
+        return redirect(url_for("main.index"))
+
+
+
+@admin.route("/delete-genre/<genre_id>")
+def delete_genre(genre_id):
+    """
+    Delete Genre View
+
+    Queries mongoDB Genre collection and deletes
+    Genre, using genre_id as the argument.
+    """
+
+    if session["user"] == "admin":
+
+        try:
+            Track.delete_genre(genre_id)
+            flash("Genre deleted successfully")
+            return redirect(url_for("admin.manage_genres"))
+        except:
+            flash("Something went wrong. Please try again")
+            return redirect(url_for("admin.manage_genres"))
+    
