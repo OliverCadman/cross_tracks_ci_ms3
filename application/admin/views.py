9677436da42d3af2bfaf5c7e293206576b18ddf3
Oliver Cadman <o.cadman@live.co.uk>
2021-12-10 10:20:12 +0000

    Added new module 'admin' with init file and views file. Initialize admin blueprint with 'manage_genres' view, rendering 'manage-genres.html'

---
+++ application/admin/views.py
@@ -0,0 +1,9 @@
+from flask import (Blueprint, render_template, redirect,
+                   url_for, session)
+from application.tracks.classes import Track
+
+admin = Blueprint('admin', __name__)
+
+@admin.route("/manage-genres")
+def manage_genres():
+    return render_template('manage-genres.html')
