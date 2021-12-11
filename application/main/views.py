c4a024ab9d7af987b14c9112f9c348763e6f8b9e
Oliver Cadman <o.cadman@live.co.uk>
2021-12-11 17:04:09 +0000

    Added comments to views and functions

--- application/main/views.py
+++ application/main/views.py
@@ -1,3 +1,21 @@
+"""
+Main: Sub-module
+
+Handles views for Home Page and Contact Page
+
+Handles data from form input in Contact Page
+and sends email to website owner.
+
+Views:
+    index()
+
+    contact()
+
+Functions:
+    send_email()  
+"""
+
+
 from flask import render_template, url_for, Blueprint, request, flash, redirect
 from application import mailing
 from flask_mail import Message
@@ -5,19 +23,32 @@ import os
 
 main = Blueprint("main",__name__)
 
+
 @main.route("/")
 @main.route("/index")
 def index():
+    """
+    Renders the website's home page.
+    """
     return render_template("index.html")
+
 
 @main.route("/contact", methods=["GET", "POST"])
 def contact():
-        
+    """
+    Renders the website's contact page.
+    """  
     return render_template("contact.html")
+
 
 @main.route("/send_email", methods=["GET", "POST"])
 def send_email():
-
+    """
+    Handles data submitted from form in
+    Contact page. Flask Mail utilised 
+    to send email to website owner, in
+    html format ("contact-message.html")
+    """
     if request.method == "POST":
 
         sender = request.form.get("full_name")
@@ -37,4 +68,3 @@ def send_email():
         except: 
             flash('Sorry, something went wrong. Please try again')
             return redirect(url_for('main.contact'))
-
