867b005783aa30200c21d2e210042d20df382b4b
Oliver Cadman <o.cadman@live.co.uk>
2021-12-21 10:53:36 +0000

    Cleaned up code for PEP8

--- run.py
+++ run.py
@@ -2,7 +2,7 @@
 Cross//Tracks run.py
 =====================
 
-Calls the instantiated Flask application from 
+Calls the instantiated Flask application from
 "application/__init__.py, and commands the server
 to run the instance.
 "
@@ -15,11 +15,9 @@ from application.config import Config
 
 app = create_app()
 
-# Run the app with IP and PORT variables from 
+# Run the app with IP and PORT variables from
 # env.py file
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
             debug=False)
-
-
