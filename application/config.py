44a4a946b9fbf20b76d4448a299e0ed66145a69b
Oliver Cadman <o.cadman@live.co.uk>
2021-12-21 10:52:08 +0000

    Cleaned up code for PEP8

--- application/config.py
+++ application/config.py
@@ -1,6 +1,6 @@
 """
 Sets the configuration variables needed for
-the application to run. 
+the application to run.
 
 class 'Config' passed into create_app()
 function in "application?__init__.py"
@@ -11,22 +11,17 @@ import os
 if os.path.exists("env.py"):
     import env
 
+
 class Config:
     # MongoDB config
     SECRET_KEY = os.environ.get("SECRET_KEY")
     MONGO_URI = os.environ.get("MONGO_URI")
     MONGO_DBNAME = os.environ.get("MONGO_DBNAME")
-    
+
+    # Flask Mail Config
     MAIL_SERVER = os.environ.get("MAIL_SERVER")
     MAIL_PORT = os.environ.get("MAIL_PORT")
     MAIL_USE_TLS = os.environ.get("MAIL_USE_TLS")
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
     SENDINBLUE_API_KEY = os.environ.get("SENDINBLUE_API_KEY")
-    
-
-
-
-
-
-
